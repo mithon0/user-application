a0675bec5e64fdedd92201de9efb57f8bbab2f54
mithon0 <mithonhossen7244@gmail.com>
2023-08-27 05:32:38 +0600

    user interface done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,130 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import Card from './Card/Card';
 
 function App() {
+  const [user,setUser]=useState([]);
+
+  useEffect(()=>{
+
+    fetch('user.json')
+    .then(res=>res.json())
+    .then(data=>setUser(data))
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+       <div className='flex hh gap-3'>
+       <section>
+       <div className='w-[450px] mx-auto border bg-slate-300 '>
+          <div className='flex items-center justify-center bg-slate-300 gap-72 pt-2 sticky top-0'>
+           <div className='flex items-center gap-2 '>
+            <div style={{ "border-radius":" 10px 0px 0px 10px"}} className='w-5 h-5  bg-red-700 '></div> <span className='text-xl font-medium '> Incomplete</span>
+            </div> 
+            <div className='w-5 h-5 bg-slate-400 flex  justify-center rounded-md  text-white  '>
+                <h3 className='pb-1'>0</h3>
+            </div>
+        </div>
+       <div className='snap-y  scroll-p-0.5 scroll-smooth ss'>
+       {
+          user.map(users=><Card
+          key={users.name}
+          users={users}
+          ></Card>)
+        }
+       </div>
+      
+      </div>
+       </section>
+       <section>
+       <div className='w-[450px] mx-auto border bg-slate-300 '>
+          <div className='flex items-center justify-center bg-slate-300 gap-72 pt-2 sticky top-0'>
+           <div className='flex items-center gap-2 '>
+            <div style={{ "border-radius":" 10px 0px 0px 10px"}} className='w-5 h-5  bg-blue-500 '></div> <span className='text-xl font-medium '> To Do</span>
+            </div> 
+            <div className='w-5 h-5 bg-slate-400 flex  justify-center rounded-md  text-white  '>
+                <h3 className='pb-1'>0</h3>
+            </div>
+        </div>
+       <div className='snap-y  scroll-p-0.5 scroll-smooth ss'>
+       {
+          user.map(users=><Card
+          key={users.name}
+          users={users}
+          ></Card>)
+        }
+       </div>
+      
+      </div>
+       </section>
+       <section>
+       <div className='w-[450px] mx-auto border bg-slate-300 '>
+          <div className='flex items-center justify-center bg-slate-300 gap-72 pt-2 sticky top-0'>
+           <div className='flex items-center gap-2 '>
+            <div style={{ "border-radius":" 10px 0px 0px 10px"}} className='w-5 h-5  bg-yellow-300 '></div> <span className='text-xl font-medium '> Doing</span>
+            </div> 
+            <div className='w-5 h-5 bg-slate-400 flex  justify-center rounded-md  text-white  '>
+                <h3 className='pb-1'>0</h3>
+            </div>
+        </div>
+       <div className='snap-y  scroll-p-0.5 scroll-smooth ss'>
+       {
+          user.map(users=><Card
+          key={users.name}
+          users={users}
+          ></Card>)
+        }
+       </div>
+      
+      </div>
+       </section>
+       <section>
+       <div className='w-[450px] mx-auto border bg-slate-300 '>
+          <div className='flex items-center justify-center bg-slate-300 gap-72 pt-2 sticky top-0'>
+           <div className='flex items-center gap-2 '>
+            {/* <div style={{ "border-radius":" 10px 0px 0px 10px"}} className='w-5 h-5  bg-yellow-300 '></div> */}
+             <span className='text-xl font-medium '> Under Review</span>
+            </div> 
+            <div className='w-5 h-5 bg-slate-400 flex  justify-center rounded-md  text-white  '>
+                <h3 className='pb-1'>0</h3>
+            </div>
+        </div>
+       <div className='snap-y  scroll-p-0.5 scroll-smooth ss'>
+       {
+          user.map(users=><Card
+          key={users.name}
+          users={users}
+          ></Card>)
+        }
+       </div>
+      
+      </div>
+       </section>
+       <section>
+       <div className='w-[450px] mx-auto border bg-slate-300 '>
+          <div className='flex items-center justify-center bg-slate-300 gap-72 pt-2 sticky top-0'>
+           <div className='flex items-center gap-2 '>
+            {/* <div style={{ "border-radius":" 10px 0px 0px 10px"}} className='w-5 h-5  bg-yellow-300 '></div>  */}
+            <span className='text-xl font-medium '> Completed</span>
+            </div> 
+            <div className='w-5 h-5 bg-slate-400 flex  justify-center rounded-md  text-white  '>
+                <h3 className='pb-1'>0</h3>
+            </div>
+        </div>
+       <div className='snap-y  scroll-p-0.5 scroll-smooth ss'>
+       {
+          user.map(users=><Card
+          key={users.name}
+          users={users}
+          ></Card>)
+        }
+       </div>
+      
+      </div>
+       </section>
+       </div>
+    
+    
   );
 }
 
---
+++ src/Card/Card.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {  FaCalendar, FaComments, FaLayerGroup, FaPaperclip } from "react-icons/fa";
+const Card = ({users}) => {
+    const {name,pic,comment,date}=users;
+    return (
+     
+      <div className='bg-white mt-5 mb-5 pt-2 pb-2 rounded mx-1 px-1 '>
+      <div className='flex gap-36 items-center'>
+      <div className='flex gap-2 items-center'>
+          <img className='w-10 h-10 rounded-full' src={pic} alt="" />
+          <h2>{name}</h2>
+      </div>
+      <div className='flex gap-2 items-center'>
+          <img className='w-10 h-10 rounded-full' src="https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80" alt="" />
+          <h2>Istiaq ahmed</h2>
+      </div>
+      </div>
+      <div className='flex gap-16 mt-4 '>
+          <div className='flex gap-1 items-center text-slate-500'> <FaLayerGroup/><p className='flex-wrap text-slate-500'>Lorem ipsum dolor sit, amet consectetur </p></div>
+          <div className='flex gap-1 items-center text-slate-500'>
+              <FaCalendar/><p className='flex-wrap text-slate-500'>1/2</p>
+          </div>
+
+      </div>
+      <div className='mt-5 flex gap-7 mx-auto justify-center'>
+      <div>
+          <img className='w-7 h-7 rounded-full' src="https://images.unsplash.com/photo-1531891437562-4301cf35b7e4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1528&q=80" alt="" />
+      </div>
+      <div>
+          <img className='w-7 h-7 rounded-full' src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1376&q=80" alt="" />
+      </div>
+      <div className='w-7 h-7 flex items-center rounded-full bg-slate-500'>
+          12+
+      </div>
+      <div className='flex items-center gap-2 text-slate-600'>
+          <FaComments/><span>{comment}</span>
+      </div>
+      <div>
+      <input  type="file" id="my_file_input" style={{display:"none"}}/>
+<label className='flex items-center text-slate-600 gap-2' htmlFor="my_file_input">
+ <FaPaperclip/><span>{"25"}</span>
+</label>
+      </div>
+      <div className='flex items-center gap-2 text-slate-600'>
+          <FaCalendar/><span>{date}</span>
+      </div>
+      </div>
+      </div>
+    );
+};
+
+export default Card;
